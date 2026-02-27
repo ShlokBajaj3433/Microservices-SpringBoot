b1bca99d6031a922022a81b7f5f7a395e9133ed5
Shlok Bajaj <shlokpbajaj000@gmail.com>
2026-02-28 00:54:11 +0530

    feat: enhance order service with Kafka integration, add OrderPlacedEvent, and update Docker configurations

--- SPRINGBOOT-Microservices/order-service/src/main/java/com/orderservice/order_service/services/OrderService.java
+++ SPRINGBOOT-Microservices/order-service/src/main/java/com/orderservice/order_service/services/OrderService.java
@@ -3,16 +3,21 @@ package com.orderservice.order_service.services;
 import java.math.BigDecimal;
 import java.util.UUID;
 
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.orderservice.order_service.client.InventoryClient;
 import com.orderservice.order_service.dto.OrderRequest;
+import com.orderservice.order_service.event.OrderPlacedEvent;
 import com.orderservice.order_service.model.Order;
 import com.orderservice.order_service.repository.OrderRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -20,6 +25,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest) {
         
@@ -32,7 +38,14 @@ public class OrderService {
             order.setSkuCode(orderRequest.skuCode());
             order.setQuantity(orderRequest.quantity());
             orderRepository.save(order);
-        } else {
+         
+            // Kafka producer code can be added here to send order details to a topic for further processing 
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber() , orderRequest.userDetails().email());
+            log.info("Start: Send order placed event to Kafka topic: {}", orderPlacedEvent);
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("End: Send order placed event to Kafka topic: {}", orderPlacedEvent);
+        }
+        else {
             throw new IllegalArgumentException("Product is not in stock, please try again later");
         }
     }
